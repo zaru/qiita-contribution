f03a89b3d5d79fd157239527851c7bda75d2615c
zaru@sakuraba <zarutofu@gmail.com>
2015-08-20 23:15:04 +0900

    D3jsでカレンダーサンプルを表示

--- app.rb
+++ app.rb
@@ -1,20 +1,34 @@
-require 'sinatra'
-require 'sinatra/reloader'
+class WebApp < Sinatra::Base
 
-qiita_config = {
-  :qiita_state => 'haue73aueaoakdjfh13yehd934g46dh',
-  :qiita_client_id => 'bb80979a6790e220449e26cf0f9dd3a502af6f4b',
-  :qiita_scope => 'read_qiita'
-}
-before do
-  @qiita_config = qiita_config
-end
+  register Sinatra::AssetPack
 
-get '/' do
-  slim :index
-end
+  qiita_config = {
+    :qiita_state => 'haue73aueaoakdjfh13yehd934g46dh',
+    :qiita_client_id => 'bb80979a6790e220449e26cf0f9dd3a502af6f4b',
+    :qiita_scope => 'read_qiita'
+  }
 
-get '/auth_complete' do
-  @code = request['code']
-  slim :auth_complete
+  assets do
+    serve '/js', from: 'bower_components/'
+
+    js :application, [
+                     '/**/*.js'
+                   ]
+
+    js_compression :jsmin
+  end
+
+  before do
+    @qiita_config = qiita_config
+  end
+
+  get '/' do
+    slim :index
+  end
+
+  get '/auth_complete' do
+    @code = request['code']
+    slim :auth_complete
+  end
+
 end
